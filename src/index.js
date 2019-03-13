72f1509fe5144b55a71c0f8799a8a482598894a8
Ilya Kiryliuk <ilya.ey@mail.ru>
2019-03-13 17:56:26 +0300

    resolved

--- src/index.js
+++ src/index.js
@@ -1,41 +1,31 @@
 module.exports = function getZerosCount(number, base) {
-  var length = 0;
-  var array_divider = [length];  //массив делителей base
-  var array1 = [length];         //степень делителя 
-  var array2 = [length];         //сколько раз делитель встречается в number
-  var number_1, number_2;        //вспомогательные "number"
-  var zerosCount;
-  
-  for(i = 2; i <= base; i++){           // делитель и его степень
-    if((base / i) % 1 == 0){
-      length++;
-      array1[length - 1] = 0;
-      array_divider[length - 1] = i;
-      while((base / i) % 1 == 0){
-        base = base / i;
-        array1[length - 1]++;
+  var zerosCount = number;
+  var power;  
+  var i;  
+  var dividerCount;
+  var number_1;           //вспомогательный number
+
+  for (divider = 2; divider <= base; divider++) {
+    if ((base / divider) % 1 == 0) {
+      power = 0;
+      while((base / divider) % 1 == 0){   //находим степень делителя
+        power = power + 1;
+        base = base / divider;
+      } 
+
+      dividerCount = 0;
+      number_1 = number;
+      i = number_1 / divider;
+      
+      while (i > 1) {         
+        dividerCount = dividerCount + Math.floor(number_1 / divider);   // находим сколько раз делитель встречается в факториале
+        number_1 = i;
+        i = i / divider;
       }
+
+      zerosCount = Math.min(zerosCount, Math.floor(dividerCount / power));  //количество нулей
     }
   }
 
-  for(i = 0; i < length; i++){           // сколько раз делитель встречается в числе
-    array2[i]=0;
-    number_1 = number;
-    for(; number_1 >= array_divider[i]; number_1--){
-      number_2 = number_1;
-      while((number_2 / array_divider[i]) % 1 == 0){
-        number_2 = number_2 / array_divider[i];
-        array2[i]++;
-      }
-    }
-  }
-  
-  zerosCount = ((array2[0] - (array2[0] % array1[0])) / array1[0]); //количество нулей
-  for(i = 1; i < length; i++){
-    if(zerosCount > ((array2[i] / array1[i]) - (array2[i] % array1[i]))){
-      zerosCount = (array2[i] / array1[i]) - (array2[i] % array1[i]);
-    }
-  }
-  
   return zerosCount;
 }
